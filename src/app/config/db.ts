30873220fb52bbcb6defababce098c3f05ad2160
user <rajasingh.a@contus.in>
2020-06-20 18:48:44 +0530

    Mongo db updated

--- src/app/config/db.ts
+++ src/app/config/db.ts
@@ -1,4 +1,4 @@
-import {  MongoClient } from "https://deno.land/x/mongo@v0.7.0/mod.ts";
+import {  MongoClient } from "https://deno.land/x/mongo@v0.8.0/mod.ts";
 
 export class DatabaseConnection {
   public client: MongoClient;
